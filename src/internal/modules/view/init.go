0d3b515c1acdd5b46079219de2a3382774f689e0
Kovel Alexander <xanderkovel@gmail.com>
2023-04-23 12:11:14 +0300

    Added handle for users

--- src/internal/modules/view/init.go
+++ src/internal/modules/view/init.go
@@ -27,20 +27,43 @@ func goDotEnvVariable(key string) string {
 	return os.Getenv(key)
 }
 
-func singUp(bot *tgbotapi.BotAPI, chatId int64, update tgbotapi.Update, controller *controllers.Controller) string {
-	var reply = ""
+func singUp(chatId int64, user *UsersMessage) string {
+	addNextMessages("Введите свою фамилию", user, chatId)
+	addNextMessages("Введите свою специальность", user, chatId)
+	addNextMessages("Введите свою роль", user, chatId)
+	msg := printNewMessage(user.NextMessages)
+	return msg
+}
 
-	msg := tgbotapi.NewMessage(chatId, "Введите свою фамилию")
-	update
-	msg = tgbotapi.NewMessage(chatId, "Введите свою специальность")
+func printNewMessage(nextMessages []string) string {
+	msg := nextMessages[0]
+	nextMessages = remove(nextMessages, 0)
+	return msg
+}
 
-	msg = tgbotapi.NewMessage(chatId, "Введите свою роль")
+func handleUsers(Users []UsersMessage, chatId int64, userMessage string, controller *controllers.Controller) string {
+	var msg string
+	for _, u := range Users {
+		if u.ChatId == chatId {
+			u.UserMessages = append(u.UserMessages, userMessage)
+			if len(u.NextMessages) > 0 {
+				msg = printNewMessage(u.NextMessages)
+			} else {
+				msg = endSingUp(&u, chatId, controller)
+			}
+		}
+	}
+	return msg
+}
+
+func endSingUp(user *UsersMessage, chatId int64, controller *controllers.Controller) string {
+	var reply string
 
 	newDoctor := &auth_dto.NewDoctor{
 		TokenId:    strconv.FormatInt(chatId, 10),
-		Surname:    surname,
-		Speciality: speciality,
-		Role:       role,
+		Surname:    user.UserMessages[0],
+		Speciality: user.UserMessages[1],
+		Role:       user.UserMessages[2],
 	}
 	_, err := controller.SingUp(context.Background(), newDoctor)
 	if err != nil {
@@ -57,26 +80,37 @@ func handleBot(
 	updates tgbotapi.UpdatesChannel,
 	bot *tgbotapi.BotAPI) {
 
+	// ToDO:Сделать массив юзеров (это должна быть структура)
+	// добавить обработку этого массива и в зависимости от поля запросы делать выводы.
+
+	var Users []UsersMessage
+
 	for update := range updates {
 		if update.Message != nil {
 
 			ChatId := update.Message.Chat.ID
 			msg := tgbotapi.NewMessage(ChatId, update.Message.Text)
 
-			switch update.Message.Text {
-			case "help":
-				msg.Text = "Type /singup"
-			case "singup":
-				msg.Text = singUp(bot, ChatId, updates, controller)
-			case "status":
-				msg.Text = "I'm ok."
-			case "open":
-				msg.ReplyMarkup = numericKeyboard
-			case "close":
-				msg.ReplyMarkup = tgbotapi.NewRemoveKeyboard(true)
-			default:
-				msg.Text = "Ты кринж"
+			if len(Users) > 0 {
+				msg.Text = handleUsers(Users, ChatId, update.Message.Text, controller)
+			} else {
+				switch update.Message.Text {
+				case "help":
+					msg.Text = "Type /singup"
+				case "singup":
+					Users = append(Users, UsersMessage{ChatId: ChatId, Command: update.Message.Text})
+					msg.Text = singUp(ChatId, &Users[len(Users)-1])
+				case "status":
+					msg.Text = "I'm ok."
+				case "open":
+					msg.ReplyMarkup = numericKeyboard
+				case "close":
+					msg.ReplyMarkup = tgbotapi.NewRemoveKeyboard(true)
+				default:
+					msg.Text = "Ты кринж"
+				}
 			}
+
 			if _, err := bot.Send(msg); err != nil {
 				panic(err)
 			}
